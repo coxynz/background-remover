5c9c6682be19ec32b4a8a54d5b511eee76a88581
coxynz <coxy576@pm.me>
2026-02-25 12:08:53 +1300

    github pages optimise

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/background-remover/',
   optimizeDeps: {
     exclude: ['@imgly/background-removal'],
   },
@@ -11,3 +12,4 @@ export default defineConfig({
     },
   },
 });
+
